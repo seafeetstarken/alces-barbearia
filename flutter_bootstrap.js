6ac427a17288429b04de8362decdc6c3b9f407f8
ferramentastecnologia <ferramentastecnologia@users.noreply.github.com>
2026-06-22 16:54:53 +0000

    deploy: ebd2fab30880f6ce1809e9cae1bd44941b56a5ca

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "621035593" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "77250520" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
